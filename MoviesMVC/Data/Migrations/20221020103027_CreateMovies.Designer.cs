76186786ca90d9890b71188316307e725a784842
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-10-20 11:41:41 +0100

    Movie MVC nearly done just need screenshots

--- MoviesMVC/Data/Migrations/20221020103027_CreateMovies.Designer.cs
+++ MoviesMVC/Data/Migrations/20221020103027_CreateMovies.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesMVC.Data;
 
@@ -11,9 +12,10 @@ using MoviesMVC.Data;
 namespace MoviesMVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221020103027_CreateMovies")]
+    partial class CreateMovies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
